316a29ab565d620c064f4525e19368a9a43f70ae
Jonar Guttorm <jonar98@hotmail.no>
2020-05-08 11:27:18 +0200

    Co-authored-by: Luca Frediani <luca.frediani@gmail.com> Co-authored-by: Roberto Di Remigio <roberto.diremigio@gmail.com>

--- bachelor.py
+++ bachelor.py
@@ -8,38 +8,10 @@ This is a temporary script file.
 import numpy as np
 import quadpy
 
-
-# The lebedev sphere
-
-#scheme.show()
-
-#print("Weights = ",scheme.weights) # Shows the weights for the sphere
-#print('Lebedev Degree:', scheme.degree)
-##########################
-#Paramaters:
-
-
-
-
-k = 1.0694
-R = 2 # Radius of the sphere
-q = 1 # Charge
-xyz_sphere = [1, 1, 1] # Center position of the sphere
-xyz_charge = [0, 0, 0] # Position of the charge
-
-#Lebedev Quadrature
-###
-scheme = quadpy.sphere.lebedev_003a ()    # Which precision of Lebedev
-grid_areals = scheme.integrate(lambda x: 1, xyz_sphere, R)
-
-
-
 def sphere_size(points, radius):
     #Changes the radius of the sphere
     new_radius = points * radius
     return new_radius
-
-points = sphere_size(scheme.points, R)
 
 def sphere_position(points, new_position):
     # Changes the center position of the sphere from [0,0,0] to new_position
@@ -48,37 +20,10 @@ def sphere_position(points, new_position):
         x[1] = x[1]+new_position[1]
         x[2] = x[2]+new_position[2]
     return points
-    
-
-points = sphere_position(points, xyz_sphere)
-
-
-
-print("Points on the sphere:")
-print(points)
-print(points.shape[0])
-
-
 
 def total_areal(grid_areals):
     result = sum(grid_areals)
     return result
-
-total_areal = total_areal(grid_areals)
-print ('Total areal:', total_areal)
-
- 
-print("Weights = ",grid_areals)
-
-###
-
-w_i = scheme.weights * total_areal
-print(w_i.shape[0])
-
-##
-#Cosmo
-################################################
-
 
 def S(points, weights):
     # Create a square matrix
@@ -92,11 +37,6 @@ def S(points, weights):
                 s[i, j] = ( 1 / np.linalg.norm(p-q))
     return s
 
-
-S_matrix = S(points,w_i)
-
-
-################################################
 def potential(points, position_charge, charge):
     # Create array for the potential
     V = np.zeros(points.shape[0]) #matches number of lebedev points
@@ -106,12 +46,6 @@ def potential(points, position_charge, charge):
             V[i] += charge[A] / np.linalg.norm(x-R)
     return V
 
-r_i = potential(points, [xyz_charge], [q])
-print("r_i", r_i)
-
-
-
-# s_ij_inverted -> S_matrix_inverted
 def solve(S, V):
     # Invert S
     S_matrix_inverted = np.linalg.inv(S)
@@ -119,8 +53,38 @@ def solve(S, V):
     sigma = np.dot(S_matrix_inverted, V)
     return sigma
 
-Sigma = solve(S_matrix, r_i)
+#print("Weights = ",scheme.weights) # Shows the weights for the sphere
+#print('Lebedev Degree:', scheme.degree)
+##########################
+#Paramaters:
 
-print(Sigma)
-print(np.sum(Sigma))
+k = 1.0694
+R = 2 # Radius of the sphere
+q = np.array([+2, -1]) # Charge
+xyz_sphere = [0, 0, 0] # Center position of the sphere
+xyz_charge = np.array([[0, 0, -1], [0,0, +1]]) # Position of the charge
+#Lebedev Quadrature
+###
+# q1 = 2 q2 = -1
+# (0, 0, -1)  (0, 0, +1)
+#
+scheme = quadpy.sphere.lebedev_031 ()    # Which precision of Lebedev
+grid_areals = scheme.integrate(lambda x: 1, xyz_sphere, R)
+points = sphere_size(scheme.points, R)
+points = sphere_position(points, xyz_sphere)
+print("Points on the sphere:")
+print(points)
+print(points.shape[0])
+total_areal_sphere = total_areal(grid_areals)
+print ('Total areal:', total_areal_sphere)
+print("Weights = ",grid_areals)
+w_i = scheme.weights * total_areal_sphere
+print(w_i.shape[0])
+S_matrix = S(points,w_i)
+r_i = potential(points, xyz_charge, q)
+print("r_i", r_i)
+Sigma = solve(S_matrix, r_i)
+print(f"Sigma = {Sigma}")
+print(f"Total charge = {np.sum(Sigma)}")
+
 #np.testing.assert_allclose(np.sum(Sigma), - q, rtol=1e-2)
